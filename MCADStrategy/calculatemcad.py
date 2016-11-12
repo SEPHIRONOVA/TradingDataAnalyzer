643601568a29feef124ea4cdf31a0f8de7a599ee
SEPHIRONOVA <yy1020cn@hotmail.com>
2016-11-12 16:47:53 -0500

    Added how to calculate mcad

---
+++ MCADStrategy/calculatemcad.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from StoreStrategy import storage
+
+class calculateMCAD:
+    @classmethod
+    def getMCAD(data):
+        ema26 = pd.ewma(data, span = 26)
+        ema12 = pd.ewma(data, span = 12)
+        ema09 = pd.ewma(data, span = 9)
+        mcadline = ema12 - ema26
+        mcadhistogram = mcadline-ema09
+        print(mcadline)
+        print(mcadhistogram)
+
+        return mcadline
+
+    @classmethod
+    def sigmoid(x):
+        return 1 / (1 + math.exp(-x))
