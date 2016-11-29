e33966d8d8cee5a2486e0b1c12b42d3a69a3fc60
SEPHIRONOVA <yy1020cn@hotmail.com>
2016-11-28 22:35:06 -0500

    Added a function to calculate daily profit/loss

--- Simulation/trader.py
+++ Simulation/trader.py
@@ -1,10 +1,11 @@
+from typing import Dict
 from Models.portfolio_summary import PortfolioSummary
 from Models.daily_result import DailyResult
 from Simulation.portfolio import Portfolio
 from Simulation.market_snapshot_helper import MarketSnapshotHelper
+from Simulation.daily_result_evaluator import DailyResultEvaluator
 
 __author__ = 'raymond'
-
 
 class Trader:
 	def __init__(self, trading_strategy, total_capital):
@@ -13,6 +14,7 @@ class Trader:
 		self.strategy = trading_strategy
 		self.portfolio = Portfolio()
 		self.daily_results = []
+		self.daily_result_evaluator = DailyResultEvaluator(total_capital)
 
 	def notify(self, market_snapshot):
 		market_snapshot_helper = MarketSnapshotHelper(market_snapshot)
@@ -20,7 +22,29 @@ class Trader:
 			date = market_snapshot_helper.get_date()
 			portfolio_summary = self.portfolio.evaluate(market_snapshot)
 			daily_result = DailyResult(date, self.total_capital, portfolio_summary)
+
+			print("\n")
+			print("The end of day")
+			print(daily_result.date)
+			#for Target_exposure in daily_result.portfolio_summary.target_exposures:
+			#	print(Target_exposure.ticker)
+			#	print(Target_exposure.amount)
+
+			#print("Finish printing target_exposures for today")
+
+
 			self.daily_results.append(daily_result)
+
+			print("Start printing today's profit/loss")
+
+			print(self.calculate_daily_profit_loss())
+			print("\nTotal profit/loss so far:")
+			print(sum(self.calculate_daily_profit_loss()))
+			#for c in self.calculate_daily_profit_loss():
+			#	print(c)
+
+			print("Finish printing today's profit/loss")
+
 			self.portfolio.clear()
 			self.strategy.reset()
 			self.total_capital = self.total_capital_backup
@@ -40,3 +64,19 @@ class Trader:
 			else:
 				print('DEBUG: Should never happen')
 				assert False
+
+
+
+	def calculate_daily_profit_loss(self):
+		daily_p_l = list()
+		for daily_result in self.daily_results:
+			daily_p_l.append(DailyResultEvaluator.calculate_profit_or_loss(self.daily_result_evaluator,daily_result))
+
+		return daily_p_l
+
+
+	#def calculate_daily_exposure(self):
+	#    for daily_result in self.daily_results
+	#        for Target_exposure in daily_result.portfolio_summarytarget_exposures:
+	#            for ticker in Target_exposure.ticker
+	#
