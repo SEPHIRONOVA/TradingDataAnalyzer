e01dd13c642d0837ea2e48042b51a545dc2a753e
Albert Yao <yy1020cn@hotmail.com>
2021-04-04 17:25:32 -0400

    Update Main.py
    Updated trader comparison & hyper parameters of model

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ from Simulation.daily_result_evaluator import DailyResultEvaluator
 from Simulation.sharp_ratio import SharpRatio
 from Simulation.stochastic_oscillator_strategy import StochasticOscillatorStrategy
 
-__author__ = 'raymond'
+__author__ = 'raymond & Albert'
 
 if __name__ == '__main__':
 	capital_per_stock = 100000
@@ -28,28 +28,28 @@ if __name__ == '__main__':
 	upper_bound = 0.7
 	lower_bound = 0.3
 
-	#trader1 = Trader(McadSignalLineCrossoverStrategy(initial_capital, num_of_stocks), initial_capital)
-	#trader2 = Trader(McadZeroCrossoverStrategy(initial_capital, num_of_stocks), initial_capital)
+	trader1 = Trader(McadSignalLineCrossoverStrategy(initial_capital, num_of_stocks), initial_capital)
+	trader2 = Trader(McadZeroCrossoverStrategy(initial_capital, num_of_stocks), initial_capital)
 	trader3 = Trader(StochasticOscillatorStrategy(initial_capital, num_of_stocks,look_back_period,k_percent_period,d_percent_period,upper_bound,lower_bound), initial_capital)
 
-	#market.register(trader1)
-	#market.register(trader2)
+	market.register(trader1)
+	market.register(trader2)
 	market.register(trader3)
 	market.start()
 
 	evaluator = DailyResultEvaluator(initial_capital)
 	print('Calculating daily profit and Loss: ' + format(time.clock(), '.2f') + ' secs')
-	#for daily_result in trader1.daily_results:
-	#	PnL1 = evaluator.calculate_profit_and_loss(daily_result)
-	#	print(daily_result.date)
-	#	print(PnL1)
-	#	print()
+	for daily_result in trader1.daily_results:
+		PnL1 = evaluator.calculate_profit_and_loss(daily_result)
+		print(daily_result.date)
+		print(PnL1)
+		print()
 
-	#for daily_result in trader2.daily_results:
-	#	PnL2 = evaluator.calculate_profit_and_loss(daily_result)
-	#	print(daily_result.date)
-	#	print(PnL2)
-	#	print()
+	for daily_result in trader2.daily_results:
+		PnL2 = evaluator.calculate_profit_and_loss(daily_result)
+		print(daily_result.date)
+		print(PnL2)
+		print()
 
 	for daily_result in trader3.daily_results:
 		PnL3 = evaluator.calculate_profit_and_loss(daily_result)
@@ -57,10 +57,10 @@ if __name__ == '__main__':
 		print(PnL3)
 		print()
 
-	#sharp_ratio1 = SharpRatio(trader1.daily_results, evaluator).calculate()
-	#sharp_ratio2 = SharpRatio(trader2.daily_results, evaluator).calculate()
+	sharp_ratio1 = SharpRatio(trader1.daily_results, evaluator).calculate()
+	sharp_ratio2 = SharpRatio(trader2.daily_results, evaluator).calculate()
 	sharp_ratio3 = SharpRatio(trader3.daily_results, evaluator).calculate()
 
-	#print('Sharp ratio for Signal Line: ', sharp_ratio1)
-	#print('Sharp ratio for Center Line: ', sharp_ratio2)
+	print('Sharp ratio for Signal Line: ', sharp_ratio1)
+	print('Sharp ratio for Center Line: ', sharp_ratio2)
 	print('Sharp ratio for Stochastic Oscillator: ', sharp_ratio3)
